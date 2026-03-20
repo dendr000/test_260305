cc23743fc8b2089c63f03c84f42b9b0fc5ceb296
dendr000 <dendro091@gmail.com>
2026-03-20 10:36:42 +0900

    Add files via upload

---
+++ Test01.sql
@@ -0,0 +1,9 @@
+SELECT * FROM PROFESSOR;
+COMMIT;
+
+SELECT NAME
+     , EMAIL
+     , SUBSTR(SUBSTR(email,INSTR(email,'@')+1),1
+     , INSTR(SUBSTR(email,INSTR(email,'@')+1),'.',-1)-1) "DOMAIN"
+  FROM professor
+ WHERE EMAIL IS NOT NULL;
